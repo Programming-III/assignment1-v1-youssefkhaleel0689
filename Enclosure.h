d8e9d10c4e78bb0258b1315498fc32d9c9011711
youssefkhaleel0689 <youssef.khaleel@student.giu-uni.de>
2025-11-12 15:05:31 +0200

    Update Enclosure.h with new includes and methods
    Refactor Enclosure class header and include directives.

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,23 @@
+#pragma once
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
+#include "ANIMAL.H"
+#include <iostream>
+using namespace std;
+#include <string>
 
-#include "Animal.h"
+class Enclosure :public Animal  {
+private:
+	Animal* ann;
+	int capacity;
+	int currentCount;
+public :
+	void addAnimal(Animal * a);
+	void displayAnimal();
+	Enclosure(Animal* ann,int capacity, int currentCount);
+	Enclosure();
+	~Enclosure();
 
-//define enclosure class here 
 
-
-
-
-
-
-
-
-#endif
+};
+#endif // !ENCLOSURE_H
